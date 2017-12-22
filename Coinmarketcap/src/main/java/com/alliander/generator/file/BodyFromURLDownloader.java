294aa7cb8739086edf4d3b428aaf50b93b45b17a
Milan <jansen.milan@gmail.com>
2017-12-22 19:26:30 +0100

    Deleted not needed code

---
+++ Coinmarketcap/src/main/java/com/alliander/generator/file/BodyFromURLDownloader.java
@@ -0,0 +1,79 @@
+package com.alliander.generator.file;
+
+import java.io.*;
+import java.net.URL;
+import javax.net.ssl.HttpsURLConnection;
+import javax.net.ssl.SSLContext;
+
+import com.alliander.webjob.MessageLogger;
+
+public class BodyFromURLDownloader {
+	/** URL of wanted body */
+	private static final String URI = "https://coinmarketcap.com";
+	/** Path plus filename plus extension to target location */
+	private static final String FILENAME_PATH = "D:\\Repo\\WebJobTemplate\\WebJobTemplate\\src\\main\\java\\com\\alliander\\generator\\file\\coinmarketcap.html";
+	
+	/**
+	 * WSDL public-lighting:
+	 * ../osgp-adapter-ws-publiclighting/wsdl/PublicLighting/
+	 * PublicLightingAdHocManagement.wsdl WSDL smart-metering-monitoring:
+	 * ../osgp-adapter-ws-smartmetering/src/main/webapp/WEB-INF/wsdl/
+	 * smartmetering/SmartMeteringMonitoring.wsdl WSDL
+	 * smart-metering-management:
+	 * ../osgp-adapter-ws-smartmetering/src/main/webapp/WEB-INF/wsdl/
+	 * smartmetering/SmartMeteringManagement.wsdl
+	 */
+
+	public static void main(String[] args) throws Exception {
+		HttpsURLConnection con;
+		con = createConnection();
+		/** Uncomment when SSL verification is needed */
+		/**
+		 * SSLContext sslContext;
+		 * SSLManager sslManager = new SSLManager("ssl_config");
+		 * sslContext = sslManager.initSslContext();
+		 * con = createConnection(sslContext);
+		 */		
+		generateFileFromUrl(con, FILENAME_PATH);
+	}
+
+	private static void generateFileFromUrl(HttpsURLConnection con, String filename) {
+		try {
+			java.io.BufferedInputStream in = new java.io.BufferedInputStream(con.getInputStream());
+			java.io.FileOutputStream fos = new java.io.FileOutputStream(filename);
+			java.io.BufferedOutputStream bout = new BufferedOutputStream(fos, 1024);
+			byte[] data = new byte[1024];
+			int i = 0;
+			while ((i = in.read(data, 0, 1024)) >= 0) {
+				bout.write(data, 0, i);
+			}
+			bout.close();
+			in.close();
+			MessageLogger.infoLogger("Succeeded to create/update file");
+		} catch (Exception e) {
+			MessageLogger.errorLogger(e, "Failed to create/update file");
+		}
+	}
+
+	private static HttpsURLConnection createConnection(SSLContext sslContext) {
+		HttpsURLConnection con = createConnection();
+		con.setSSLSocketFactory(sslContext.getSocketFactory());
+		return con;
+	}
+
+	private static HttpsURLConnection createConnection() {
+		HttpsURLConnection con;
+		try {
+			con = (HttpsURLConnection) new URL(URI).openConnection();
+		} catch (IOException e) {
+			MessageLogger.errorLogger(e, "Failed to create a connection");
+			return null;
+		}
+		con.setDoInput(true);
+		con.setDoOutput(true);
+		con.setRequestProperty("Accept-Encoding", "text/xml");
+		con.setRequestProperty("Content-Type", "text/xml;charset=UTF-8");		
+		MessageLogger.infoLogger("Succeeded to create a connection");
+		return con;
+	}
+}
--- WebJobTemplate/src/main/java/com/alliander/webjob/IWebServiceDataRequester.java
+++
@@ -1,8 +0,0 @@
-package com.alliander.webjob;
-
-public interface IWebServiceDataRequester {
-
-	/** Requests data from a web service */
-	public <T1 extends Object, T2 extends Object> T2 requestData(T1 requestObject);
-
-}
